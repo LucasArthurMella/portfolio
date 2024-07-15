c9ad1edfcbbbdc0b60575fcf47ecc4654c2afd85
LucasArthurMella <lucasmella16@gmail.com>
2024-07-14 23:17:30 -0300

    Terminado mais algumas reajustes

--- src/public/javascripts/home.ts
+++ src/public/javascripts/home.ts
@@ -3,17 +3,13 @@ import '@fancyapps/ui/dist/fancybox/fancybox.css';
 import 'slick-carousel/slick/slick.min.js';
 import $ from 'jquery';
 
-import { Carousel } from "@fancyapps/ui"
-import "@fancyapps/ui/dist/carousel/carousel.css";
-
-
 
 export function home() {
 
 
-//Fancybox.bind('[data-fancybox="gallery2"]', {
-//  //
-//});
+Fancybox.bind('[data-fancybox]', {
+  //
+});
 
 $(function(){
   $('.project-box-images').slick({
