48139122ea97ab677cba8075e1c1dc531358c9d8
LucasArthurMella <lucasmella16@gmail.com>
2024-07-16 23:04:36 -0300

    Terminei o burger mobile

--- src/public/javascripts/home.ts
+++ src/public/javascripts/home.ts
@@ -20,6 +20,33 @@ $(function(){
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    const burger = document.querySelector('#main-burguer')!;
+    const dashboard = document.querySelector('.main-nav-box2-flex')!;
+
+    burger.addEventListener('click', function() {
+        dashboard.classList.toggle('main-nav-open');
+    });
+
+    const anchorsDiv = document.querySelector(".main-nav-box2")!;
+    const anchors = anchorsDiv.querySelectorAll("*");
+
+    for(let anchor of anchors){
+      console.log(anchor);
+      anchor.addEventListener("click", () => {
+        let burguerContainer = document.querySelector(".main-nav-box2-flex")!;
+        if (burguerContainer.classList.contains("main-nav-open")) {
+          burguerContainer.classList.toggle("main-nav-open");
+        }
+      });
+    }
+
+});
+
+
+
+
 
 }
 
