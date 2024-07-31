7b7a692de54182fce619ec36dbe8e0d313bf27a7
LucasArthurMella <lucasmella16@gmail.com>
2024-07-30 23:56:12 -0300

    Progresso na aba individual de projeto

---
+++ src/public/javascripts/project.ts
@@ -0,0 +1,49 @@
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+import 'slick-carousel/slick/slick.min.js';
+import $ from 'jquery';
+
+
+
+function project(){
+Fancybox.bind('[data-fancybox]', {
+});
+
+$(function(){
+  $('.project-box-images').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.project-box-images-slider'
+  });
+});
+
+
+$(function(){
+  $('.project-box-images-slider').slick({
+   slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.project-box-images',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1
+      }
+    },
+  ]
+
+
+
+  });
+});
+
+}
+
+project();
